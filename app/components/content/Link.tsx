eaa77d1a27c28b5386b1ad594a14e768df679563
n8b8dy <ruslanchernomorski@gmail.com>
2023-02-28 22:06:53 +0200

    ui/ux improvement for Link.tsx

--- app/components/content/Link.tsx
+++ app/components/content/Link.tsx
@@ -10,8 +10,12 @@ export const Link: FC<LinksProps> = ({ children, href }) => {
       'shadow-[inset_0_0_0_0_neutral-200]',
       'text-white',
       'transition-color duration-500 ease-in-out',
+      'border-neutral-400',
       'z-10',
 
+      '[&:focus:not(:focus-visible)]:border-b-2',
+      'focus-visible:shadow-[inset_100px_0_0_0_white]',
+      'focus-visible:text-black',
       'hover:shadow-[inset_100px_0_0_0_white]',
       'hover:text-black',
     )} href={href}>{children}</a>
